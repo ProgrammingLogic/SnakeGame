1790e06ebeb26256f4a581cf15a22cddda55bb4b
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-16 22:37:03 -0600

    Add setup.py for SnakeGame

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='SnakeGame',
+    version='0.1',
+    packages=['snakegame'],
+    url='https://github.com/yourusername/SnakeGame',
+    license='MIT',
+    author='Jonathyn Stiverson',
+    author_email='jstiverson2002@gmail.com',
+    description='A simple Snake game',
+    install_requires=[
+        'pygame',
+    ],
+)
