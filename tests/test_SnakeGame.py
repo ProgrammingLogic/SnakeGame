c09e716bd51591385f0bd966b6afbbb9c4c39648
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-16 23:41:59 -0600

    Refactor SnakeGame unit tests

--- tests/test_SnakeGame.py
+++ tests/test_SnakeGame.py
@@ -1,13 +1,23 @@
-import pytest
 import unittest
 from src.SnakeGame.SnakeGame import SnakeGame
 
 
-class TestSnakeGame:
+class TestSnakeGame(unittest.TestCase):
+    def test_process_arguments(self):
+        # Ensure that the debug argument is set to False by default
+        game = SnakeGame()
+        game.process_arguments()
+        self.assertTrue(game.debug == False)
+
+        # Ensure that the debug argument is set to True when debug=True is passed as a keyword argument
+        kwargs = {"debug": True}
+        game.process_arguments(**kwargs)
+        self.assertTrue(game.debug == True)
+
     def test_start(self):
         game = SnakeGame()
         game.start()
-        # Add assertions to check if the game logic has started correctly
+        self.assertTrue(game.running)
 
     def test_update(self):
         game = SnakeGame()
@@ -19,10 +29,10 @@ class TestSnakeGame:
         game.render()
         # Add assertions to check if the game graphics have been rendered correctly
 
-    def test_handle_input(self):
+    def test_quit(self):
         game = SnakeGame()
-        game.handle_input()
-        # Add assertions to check if the user input has been handled correctly
+        game.quit()
+        # Add assertions to check if the game has been quit correctly
 
 
 if __name__ == "__main__":
