5fbdb81da81adf1472474b3e7161f85bd43d427e
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-16 23:49:59 -0600

    Add logging functionality to SnakeGame class

--- src/SnakeGame/SnakeGame.py
+++ src/SnakeGame/SnakeGame.py
@@ -1,4 +1,5 @@
 import pygame
+import logging
 
 class SnakeGame:
     """
@@ -20,17 +21,47 @@ class SnakeGame:
     """
     def __init__(self, *args, **kwargs):
         self.process_arguments(*args, **kwargs)
+        self.setup_logging()
         self.setup_pygame()
         self.start()
 
-
+   
     def process_arguments(self, *args, **kwargs):
         # Process arguments
-        if "debug" in kwargs:
-            self.debug = kwargs["debug"]
+        if "log_level" in kwargs:
+            self.process_log_level(kwargs["log_level"])
         else:
-            self.debug = False
+            self.log_level = logging.ERROR
 
+    def process_log_level(self, log_level):
+        log_levels = {
+            "DEBUG": logging.DEBUG,
+            "INFO": logging.INFO,
+            "WARNING": logging.WARNING,
+            "ERROR": logging.ERROR,
+            "CRITICAL": logging.CRITICAL,
+        }
+
+        if log_level in log_levels.keys():
+            self.log_level = log_levels[log_level]
+        else:
+            throw = ValueError(f"""Invalid log level: {log_level}""")
+
+    def setup_logging(self):
+        # Setup logging
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+        # Create console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # Create formatter and add it to the handler
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+
+        # Add the handler to the logger
+        self.logger.addHandler(ch)
 
     def start(self):
         """
@@ -40,13 +71,11 @@ class SnakeGame:
         """
         self.running = True
 
-
     def setup_pygame(self):
         # Setup pygame
         pygame.init()
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
-
 
     def loop(self):
         # Game loop
@@ -57,18 +86,15 @@ class SnakeGame:
             # Limit the game to 60 FPS
             self.clock.tick(60)
 
-
     def quit(self):
         pygame.quit()
-
 
     def render(self):
         self.screen.fill("purple")
         pygame.display.flip()
 
-
     def update(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("Quit event detected")
+                self.logger.info("pygame.QUIT event detected.")
                 self.running = False
