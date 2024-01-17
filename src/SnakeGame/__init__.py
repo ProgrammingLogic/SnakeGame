71565971f6e50b064bc994f16c815fac8188d62c
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-16 22:02:07 -0600

    Add SnakeGame class and tests

---
+++ src/SnakeGame/__init__.py
@@ -0,0 +1,4 @@
+from .SnakeGame import SnakeGame
+# This is the __init__.py file for the SnakeGame package.
+# You can add any initialization code or import statements here.
+# This file is executed when the package is imported.
