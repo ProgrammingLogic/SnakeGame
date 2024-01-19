a77023f994ab51ef516ace88e32e45d6ffd1cfde
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-18 18:43:26 -0600

    Add command line argument support to Snake Game

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
+from argparse import ArgumentParser
+
 from Application import Application
 
 
 if __name__ == "__main__":
     """
     This is the main entry point of the Snake Game.
-    It prints 'Hello world!' to the console.
     """
-    game = Application()
+    parser = ArgumentParser(description='Play SnakeGame.')
+    parser.add_argument('configuration_file', type=str, nargs='?', default='.\\res\\settings.json', help='Path of the configuration file')
+    parser.add_argument('--log_level', choices=['info', 'debug', 'warning', 'error', 'critical'], help='Log level (info, debug, warning, error, critical)')
+
+
+    args = parser.parse_args()
+    game = Application(**vars(args))
     game.loop()
