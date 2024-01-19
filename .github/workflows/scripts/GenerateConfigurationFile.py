1e45e6b1f1bc2e856015a6e2fffe0205ccb9d58f
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-18 18:32:02 -0600

    Update path of configuration file

--- .github/workflows/scripts/GenerateConfigurationFile.py
+++ .github/workflows/scripts/GenerateConfigurationFile.py
@@ -8,10 +8,9 @@ def generate_configuration_file(path, log_level):
     with open(path, 'w') as file:
         json.dump(config, file, indent=4)
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Generate a .json configuration file.')
-    parser.add_argument('path', type=str, nargs='?', default='.\snakeGameConfiguration.json', help='Path of the configuration file')
+    parser.add_argument('path', type=str, nargs='?', default='.\\res\\settings.json', help='Path of the configuration file')
     parser.add_argument('--log_level', choices=['info', 'debug', 'warning', 'error', 'critical'], help='Log level (info, debug, warning, error, critical)')
 
     args = parser.parse_args()
@@ -24,3 +23,4 @@ if __name__ == "__main__":
         log_level = args.log_level
 
     generate_configuration_file(args.path, log_level)
+    
