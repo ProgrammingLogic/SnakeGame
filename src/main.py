b0b6c42abe3ec0ff8ee5447da9465b3c7235013d
Jonathyn Stiverson <jstiverson2002@gmail.com>
2024-01-18 20:51:27 -0600

    Rename "game" to "app"

--- src/main.py
+++ src/main.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     parser.add_argument('configuration_file', type=str, nargs='?', default='.\\res\\settings.json', help='Path of the configuration file')
     parser.add_argument('--log_level', choices=['info', 'debug', 'warning', 'error', 'critical'], help='Log level (info, debug, warning, error, critical)')
 
-
     args = parser.parse_args()
-    game = Application(**vars(args))
-    game.loop()
+
+    app = Application(**vars(args))
+    app.start()
+    app.loop()
